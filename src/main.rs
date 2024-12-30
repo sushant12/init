16fb4fb88aa3e8cc4092b9fc117ea417d98b03c6
sushant12 <sussyoung9@gmail.com>
2024-12-30 20:12:43 +0000

    initialize log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use log::info;
+use std::env;
+
+pub fn log_init() {
+    // default to "info" level, just for this bin
+    let level = env::var("LOG_FILTER").unwrap_or_else(|_| "init=info".into());
+
+    env_logger::builder()
+        .parse_filters(&level)
+        .write_style(env_logger::WriteStyle::Never)
+        .format_level(false)
+        .format_module_path(false)
+        .format_timestamp(None)
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    log_init();
+    info!("Hello, world!");
     Ok(())
 }
